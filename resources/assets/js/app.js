c3bbd2120c5341b8df9bc899899120b21afb2cb2
Fikri Nugraha <fikri.nugraha@studentpartner.com>
2018-07-20 08:43:54 +0700

    init auth baru

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Index from "./components/index";
+import Login from "./components/login";
+import Register from "./components/register";
+import Home from "./components/home";
+import Forgot from "./components/forgot";
+import Reset from "./components/reset";
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
-
-require('./bootstrap');
-
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-require('./components/Example');
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Index} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/home" component={Home} />
+            <Route path="/forgotpassword" component={Forgot} />
+            <Route path="/password/reset/:token" component={Reset} />
+        </Switch>
+    </Router>,
+    document.getElementById("app")
+);
